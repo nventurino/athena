4bb97bffb6b53fd5055f6903dd83df20af84a235
Alok Kothari <alok.kothari@ascendum.com>
2021-02-20 17:59:12 -0500

    single call transcription and emotion

--- BackEnd/MLServices.py
+++ BackEnd/MLServices.py
@@ -164,5 +164,68 @@ def emotion_recognition():
         app.log_exception(e)
         error_body = json.dumps({"parse": "transcript doesn't exist for this filename"})
         return Response(error_body, 400, content_type="application/problem+json")
+
+
+@app.route('/emotionTranscription', methods=["POST"])
+def create_emotion_transcription():
+    try:
+        content = request.args
+        job_name = content['session']
+        print(job_name)
+        audio_file = content['s3location']
+        print(audio_file)
+        global bucket
+        job_uri = "https://" + bucket + ".s3.us-east-1.amazonaws.com/"+ audio_file
+        print(job_uri)
+        transcribe.start_transcription_job(
+                TranscriptionJobName = job_name,
+                Media = {'MediaFileUri': job_uri},
+                LanguageCode = 'en-US',
+                OutputBucketName = bucket
+            )
+        while True:
+            status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+                break
+            print("Not ready yet...")
+            time.sleep(5)
+        return emotion_recognition_text(job_name)
+
+    except Exception as e:
+        app.log_exception(e)
+        error_body = json.dumps({"parse": "badly formed query or missing params or resource doesn't exist"})
+        return Response(error_body, 400, content_type="application/problem+json")
+
+def emotion_recognition_text(job_name):
+    try:
+        content_object = s3.Object(bucket, job_name + ".json")
+        print(job_name)
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        transcriptString = json_content['results']['transcripts'][0]['transcript']  #maynot exist
+        summary = create_paragraph_summary(transcriptString)
+        utterances = get_bucketed_utterances(json_content['results']['items'])
+        nuggets = detect_nuggets(json_content['results']['items'])
+        emotion_scored_utterances = score_emotion_utterances(utterances)
+        analyses = {
+            "transcript": transcriptString,
+            "summary": summary,
+            "emotion_map": emotion_scored_utterances,
+            'nuggets': nuggets
+        }
+        responseDict = {'response_dict': analyses}
+        resp = jsonify(responseDict)
+        s3object = s3.Object(s3UploadBucket, 'emotionTranscription-' + job_name + '.json')
+        s3object.put(
+                    Body=(bytes(json.dumps(analyses).encode('UTF-8')))
+        )
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        app.log_exception(e)
+        error_body = json.dumps({"parse": "transcript doesn't exist for this filename"})
+        return Response(error_body, 400, content_type="application/problem+json")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=9200, debug=True)
